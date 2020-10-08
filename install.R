6819db71f0ee28dfa5121c3696015bcd510c67f6
Tim Collart <timc@vliz.be>
2020-10-08 19:03:49 +0200

    wdpar package is broken

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages(c("rgdal","raster","mapdata","maptools","downloader","directlabels","rasterVis","XML","ncdf4","kableExtra","rjson","reshape2","RColorBrewer","dplyr","tidyr","ggplot2","wdpar"))
+install.packages(c("rgdal","raster","mapdata","maptools","downloader","directlabels","rasterVis","XML","ncdf4","kableExtra","rjson","reshape2","RColorBrewer","dplyr","tidyr","ggplot2"))
